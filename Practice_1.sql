aa1bc52a405abe3d44ca53f66ea97bac9b630404
anvh74 <150644660+anvh74@users.noreply.github.com>
2023-11-13 12:07:53 +0700

    Update Practice_1.sql

--- Practice_1.sql
+++ Practice_1.sql
@@ -6,3 +6,15 @@ AND population>120000
 Select * from CITY
 Where countrycode='JPN'
 --EX3
+Select CITY, STATE from Station
+--EX4
+Select distinct City from Station
+Where City like 'A%' 
+Or City like'E%' 
+Or City like'I%' 
+Or City like'O%'
+Or City like'U%'
+--Hoặc 
+Select distinct City from Station
+Where Substr(City,1,1) In('A','E','I','O','U')
+--EX5
