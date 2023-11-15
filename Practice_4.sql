e6f6aeb783193b30027cbf24b1ce432c31904b72
anvh74 <150644660+anvh74@users.noreply.github.com>
2023-11-15 09:43:47 +0700

    Update Practice_4.sql

--- Practice_4.sql
+++ Practice_4.sql
@@ -1,1 +1,10 @@
-
+--EX1
+Select 
+Sum( Case 
+          WHEN device_type in('laptop') then 1 Else 0
+          End) as laptop_reviews,
+Sum( CASE 
+          WHEN device_type in ('tablet','phone') then 1 Else 0
+          End) as mobile_views
+from viewership
+--EX2 
