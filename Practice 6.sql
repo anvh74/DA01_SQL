08891a92e92db03381072d5a4338afe2801a92de
anvh74 <150644660+anvh74@users.noreply.github.com>
2023-11-20 17:05:11 +0700

    Create Practice 6.sql

---
+++ Practice 6.sql
@@ -0,0 +1,21 @@
+--EX1--
+With 
+ title_by_company AS
+  (Select company_id, title, count(title) as count_title
+  from job_listings
+  Group by company_id, title)
+Select count(DISTINCT company_id) as duplicate_companies
+from title_by_company
+Where count_title>1
+
+  Hoặc
+With 
+ title_by_company AS
+  (Select company_id, title, count(title) as count_title
+  from job_listings
+  Group by company_id, title
+  Having count(title)>1)
+Select count(DISTINCT company_id) as duplicate_companies
+from title_by_company
+
+--EX2--
