e4227bad317621d1ffed784dc06ae515698cfd1b
anvh74 <150644660+anvh74@users.noreply.github.com>
2023-11-20 18:35:04 +0700

    Update Practice_6.sql

--- Practice_6.sql
+++ Practice_6.sql
@@ -19,4 +19,17 @@ With
 Select count(DISTINCT company_id) as duplicate_companies
 from title_by_company
 
---EX2--
+--<Hoặc dùng subquery in From>
+
+--EX2-- >> 2 PRODUCT có doanh thu cao nhất CỦA MỖI C ATEGORY >> Dùng rank <Source: https://stackoverflow.com/questions/70191606/sum-overpartition-by-order-by-a-b>
+ With top_spend as
+   (Select category, product, sum(spend) as total_spend,
+    rank() over (partition by category order by sum(spend) DESC) as category_ranking
+    from product_spend
+    Where Extract(year FROM transaction_date)=2022
+    Group by category, product)
+  Select category, product, total_spend from top_spend
+  Where category_ranking<=2
+
+--EX3
+  
