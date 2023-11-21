b94c79760db653768baa85ade1bc343056008c17
anvh74 <150644660+anvh74@users.noreply.github.com>
2023-11-21 17:37:40 +0700

    Update Practice_7.sql

--- Practice_7.sql
+++ Practice_7.sql
@@ -11,4 +11,36 @@ With year_spend as
  Round((curr_year_spend-lag(curr_year_spend) OVER(PARTITION BY product_id ORDER BY product_id))
  /lag(curr_year_spend) OVER(PARTITION BY product_id ORDER BY product_id)*100,2)
  as yoy_rate
- from year_spend
+ from year_spend;
+
+--EX2--
+SELECT DISTINCT card_name, 
+FIRST_VALUE(issued_amount) 
+   OVER(PARTITION BY card_name Order by issue_year, issue_month) as issued_amount
+FROM monthly_cards_issued
+ORDER BY issued_amount DESC
+
+--EX3--
+With t1 as
+(SELECT * ,
+Rank() OVER(PARTITION BY user_id ORDER BY transaction_date) as rank
+FROM transactions)
+Select user_id, spend, transaction_date
+from t1 where rank=3
+
+--EX4--
+With t1 AS
+(SELECT transaction_date, user_id,
+Count(product_id) OVER (PARTITION BY User_id 
+                  ORDER BY transaction_date DESC) 
+                  as purchase_count,
+Row_number () OVER(PARTITION BY user_id
+        ORDER BY transaction_date DESC)
+        as rank
+FROM user_transactions)
+
+Select transaction_date, user_id, purchase_count 
+from t1 where rank=1
+ORDER BY transaction_date
+
+--EX5--
