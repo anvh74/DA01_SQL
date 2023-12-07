11dd1847ed7474ff58e852f46e80145b00e2e52c
anvh74 <150644660+anvh74@users.noreply.github.com>
2023-12-07 09:52:23 +0700

    Update Practice_8.sql

--- Practice_8.sql
+++ Practice_8.sql
@@ -1,16 +1,13 @@
 --EX 1--
-With frist_order_date as
+With first_order_date as
 (
 select 
-DISTINCT customer_id, 
-min(order_date) OVER (Partition by customer_id ORDER BY order_date) as first_order
+customer_id, 
+min(order_date) OVER (Partition by customer_id ORDER BY order_date) as first_order,
+customer_pref_delivery_date
 from delivery
 )
-Select
-count(t1.customer_id) as immediate_first_order
-from Delivery as t1
-Join frist_order_date as t2 
-on t1.customer_pref_delivery_date=t2.first_order
-UNION ALL
-Select count(*) from delivery
-
+Select 
+ round(sum(Case when first_order=customer_pref_delivery_date then 1 else 0 END)
+    / count(distinct customer_id)*100.00,2) AS immediate_percentage
+  from first_order_date
