84b7c3adbe7d5c7a041fcbe928868cd3f93ee577
anvh74 <150644660+anvh74@users.noreply.github.com>
2023-12-07 09:17:11 +0700

    Update Practice_8.sql

--- Practice_8.sql
+++ Practice_8.sql
@@ -1,1 +1,16 @@
+--EX 1--
+With frist_order_date as
+(
+select 
+DISTINCT customer_id, 
+min(order_date) OVER (Partition by customer_id ORDER BY order_date) as first_order
+from delivery
+)
+Select
+count(t1.customer_id) as immediate_first_order
+from Delivery as t1
+Join frist_order_date as t2 
+on t1.customer_pref_delivery_date=t2.first_order
+UNION ALL
+Select count(*) from delivery
 
