f61ef389a1a29911fe9cb0618e152317cee2b3d2
anvh74 <150644660+anvh74@users.noreply.github.com>
2023-12-07 10:16:33 +0700

    Update Practice_8.sql

--- Practice_8.sql
+++ Practice_8.sql
@@ -1,13 +1,13 @@
 --EX 1--
-With first_order_date as
-(
-select 
-customer_id, 
-min(order_date) OVER (Partition by customer_id ORDER BY order_date) as first_order,
-customer_pref_delivery_date
-from delivery
-)
 Select 
  round(sum(Case when first_order=customer_pref_delivery_date then 1 else 0 END)
     / count(distinct customer_id)*100.00,2) AS immediate_percentage
-  from first_order_date
+from
+ (select 
+  customer_id, 
+  min(order_date) OVER (Partition by customer_id ORDER BY order_date) as first_order,
+  customer_pref_delivery_date
+  from delivery) as first_order_date
+
+--EX 2--
+
