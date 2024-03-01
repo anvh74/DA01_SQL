0fdc945f57fc70b89ac9e20d2a8a15b16dd4edcb
anvh74 <150644660+anvh74@users.noreply.github.com>
2024-03-01 12:01:06 +0700

    Update Practice_1 Other solutions.sql

--- Practice_1 Other solutions.sql
+++ Practice_1 Other solutions.sql
@@ -1,3 +1,21 @@
 --EX4:
 Select DISTINCT  city from Station    -->> result cannot contain duplicates.
 Where left(city,1) IN ('a','e','i','o','u');
+
+--EX5:
+Select DISTINCT  city from Station    -->> result cannot contain duplicates.
+Where right(city,1) IN ('a','e','i','o','u');
+
+--EX6: 
+Select DISTINCT  city from Station    -->> result cannot contain duplicates.
+Where left(city,1) NOT IN ('a','e','i','o','u');
+
+--EX10:
+Select name from Customer
+Where referee_id !=2    -->> chỉ lọc các giá trị không null
+ OR referee_id is null; -->> phải lọc riêng các giá trị không null;
+
+
+
+
+
